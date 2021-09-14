6f6225da96812db2d56644df2f36a81c31620fb7
Cameron H <cameronheadlee6@gmail.com>
2021-09-14 09:49:21 -0400

    deploying

---
+++ client/build/precache-manifest.1da97e55b8e63732ce83002421f9f761.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b69b50190fdaf66e7962c2c72a1d0ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b90e01ed51a9934ab39b",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "ab939cd627fa47b412dc",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "b90e01ed51a9934ab39b",
+    "url": "/static/js/2.68ab7ba1.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.68ab7ba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab939cd627fa47b412dc",
+    "url": "/static/js/main.954efd6c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
